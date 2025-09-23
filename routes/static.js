d53419c44088fcf0ae2611aa315e737b040e865b
carolnacher <carolnacher@gmail.com>
2025-09-23 00:38:24 -0300

    22sep

--- routes/static.js
+++ routes/static.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const invController = require("../controllers/invController")
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -23,6 +24,9 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+
+// Index route
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
