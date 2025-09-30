88e8ad1a232fcf7f7fd7a8f69ca1d074ce125406
carolnacher <carolnacher@gmail.com>
2025-09-30 01:07:50 -0300

    30sep

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,5 +1,5 @@
 // accountRoute.js
-
+const regValidate = require('../utilities/account-validation')
 const express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/accountController');
@@ -12,6 +12,13 @@ router.get('/login', utilities.handleErrors(accountController.buildLogin));
 
 router.get('/register', utilities.handleErrors(accountController.buildRegister));
 
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+);
+
 
 module.exports = router;
--- utilities/index.js
+++ utilities/index.js
@@ -2,6 +2,7 @@ const invModel = require("../models/inventory-model")
 const Util = {}
 
 
+
 /* ************************
  * Constructs the nav HTML unordered list
  ************************** */
